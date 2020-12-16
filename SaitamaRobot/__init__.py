d64f40b4a72e1dc1861a611c498efa960479436e
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-16 23:49:34 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -89,6 +89,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
+    REDIS_URL = os.environ.get('REDIS_URL', None)
     REPOSITORY = os.environ.get('REPOSITORY', "")
     
     
