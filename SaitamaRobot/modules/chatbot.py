8b05c25df74d16e6b074b8de4f5d63e2f3f2690a
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2021-02-11 19:08:33 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -137,7 +137,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Senku to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Suzuya to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
