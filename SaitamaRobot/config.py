268e422479363c8ea905746496201bbb4ece076d
HM-MODS <85007966+HM-MODS@users.noreply.github.com>
2021-10-10 13:10:34 +0530

    Update and rename sample_config.py to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -15,11 +15,11 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
+    API_ID = 8209731  # integer value, dont use ""
+    API_HASH = "635604e6b6443470a7b666968137b52d"
+    TOKEN = "2046900561:AAH524cxZoHai-QOMSkPS0wLkrJ-AbEFEtw"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2037172560  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Hell_FireDevil"
     SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
